c87a136536346758212e5d7ddaef362f5b6dec6f
Milen Orfeev <milen.orfeev@abv.bg>
2019-08-29 19:31:44 +0100

    Move old project
    Issue: #3

--- app/src/main/java/com/example/milen/hope/RankedItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.milen.hope
-
-// TODO: Exceptions, check ranks.
-class RankedItem internal constructor(val description: String, var rank: Short) {
-
-    override fun toString(): String {
-        return "$description $rank"
-    }
-}
